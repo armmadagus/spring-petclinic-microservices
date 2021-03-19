10279dc2fb7f81a3ab26b15187a221a115cb7dfa
armmadagus <sirawit_niyom@yahoo.com>
2021-03-19 08:17:32 +0700

    Update DiscoveryServerApplication.java
    Test SonarQube

--- spring-petclinic-discovery-server/src/main/java/org/springframework/samples/petclinic/discovery/DiscoveryServerApplication.java
+++ spring-petclinic-discovery-server/src/main/java/org/springframework/samples/petclinic/discovery/DiscoveryServerApplication.java
@@ -28,5 +28,9 @@ public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServerApplication.class, args);
+        for(int i = 0; i < args.length; i++) {
+            String password =args[i];
+            System.out.println("password : "+password);
+        }
 	}
 }
